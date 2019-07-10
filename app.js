4d99a5e96a3735df0fca88b2cfdcb15b99db6065
Jana Uhrich <janamu@uw.edu>
2019-07-10 16:05:10 -0700

    1 is better than nothing

--- app.js
+++ app.js
@@ -22,10 +22,15 @@ const data = {
     }
   ]
 };
-
+//GET http://localhost:5000/vegetables?name=peppers
+//GET http://localhost:5000/vegetables
 app.get("/vegetables", (req, res, next) => {
   const { vegetables } = data;
-  res.json(vegetables);
+  const vegName = req.query.name;
+  const matchedVegetables = vegetables.filter(veggie => veggie.name.includes(vegName));
+  //if /vegetables return the whole array if no match return empty array
+  let message = (!vegName) ? vegetables : matchedVegetables;
+  res.json(message);
 });
 
 app.get("/vegetables/:id", (req, res, next) => {
@@ -40,6 +45,22 @@ app.get("/vegetables/:id", (req, res, next) => {
 
   res.json(vegetable);
 });
+
+//DELETE http://localhost:5000/vegetables/toQxYtAr8
+app.delete('/vegetables/:id', (req, res, next) => { 
+  const status = 200
+  const { id } = req.params
+  const vegetable = vegetables.find(veggie => veggie.id === id);
+
+  if (!vegetable) {
+    const message = `Could not find vegetable with ID of ${id}`;
+    next({ status: 404, message });
+  }
+
+  const message = `Deleted message ${id}`
+  res.status(status).json({message})
+})
+
 
 app.post("/vegetables", helpers.validate, (req, res, next) => {
   const { vegetables } = data;
