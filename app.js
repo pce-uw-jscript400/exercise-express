33abff4b3c781680ae11d4e9efce3700429317b0
Jeremy Diaz <jeremyjdz@gmail.com>
2019-08-04 14:09:31 -0700

    Added Delete vegetable

--- app.js
+++ app.js
@@ -3,25 +3,46 @@ const express = require('express')
 const { generate: generateId } = require('shortid')
 const helpers = require('./src/helpers')
 const app = express()
+// const fs = require('fs');
+// const contents = fs.readFileSync('./data/post.json', 'utf-8')
+// const data = JSON.parse(contents)
 
 if (NODE_ENV === 'development') app.use(require('morgan')('dev'))
 app.use(require('body-parser').json())
-
+ 
 const data = {
   fruits: [],
-  vegetables: []
+  vegetables: [
+    { "id": "AEdvQm9t_", "name": "green pepper", "price": "0.99" },
+    { "id": "JA_dIbXLW", "name": "red pepper", "price": "0.99" },
+    { "id": "5TsEt-xfI", "name": "turnip", "price": "0.79" }
+  ]
 }
 
-app.get('/vegetables?name=[partial-query]', (req, res, next) => {
+app.get('/vegetables', (req, res, next) => {
   const { vegetables } = data
-  res.json(vegetables)
+  // console.log(req.query.name)
+  if(typeof req.query.name != 'undefined'){
+    response = vegetables.filter( veggie => {
+      // console.log(veggie.name)
+      if (veggie.name.includes(req.query.name)){
+        // console.log("Is this working")
+        return veggie;
+      }
+    });
+  } else {
+    response = vegetables
+  }
+  // console.log(`${response} Be something` )
+  res.json(response)
+  
 })
 
 app.get('/vegetables/:id', (req, res, next) => {
   const { vegetables } = data
   const { id } = req.params
   const vegetable = vegetables.find(veggie => veggie.id === id)
-
+  console.log(id)
   if (!vegetable) {
     const message = `Could not find vegetable with ID of ${id}`
     next({ status: 404, message })
@@ -30,24 +51,33 @@ app.get('/vegetables/:id', (req, res, next) => {
   res.json(vegetable)
 })
 
-app.post('/vegetables', helpers.validate, (req, res, next) => {
+app.delete('/vegetables/:id', (req, res, next) => {
   const { vegetables } = data
-  const vegetable = { id: generateId(), ...req.body }
-
-  vegetables.push(vegetable)
-  res.status(201).json(vegetable)
-})
-
-app.delete('/vegetables/[id]', (req, res, next) => {
-  const { vegetable } = data.id
-  vegetable.split(vegetables)
+  const { id } = req.params
+  const vegetable = vegetables.find(veggie => veggie.id === id)
+  const deleteIndex = vegetables.indexOf(vegetable)
+  if (!vegetable) {
+    const message = `Vegetable could not be found.`
+    next({ status: 404, message })
+  }
+  const response = vegetables.splice(deleteIndex, 1)
+  res.json({ status: 200, response })
 })
 
 app.put('/vegetables/[id]', (req, res, next) => {
   
 })
 
-app.get('/fruits?name=[partial-query]', (req, res, next) => {
+app.post('/vegetables', helpers.validate, (req, res, next) => {
+  const { vegetables } = data
+  const vegetable = { id: generateId(), ...req.body }
+
+  console.log(vegetable)
+  vegetables.push(vegetable)
+  res.status(201).json(vegetable)
+})
+
+app.get('/fruits', (req, res, next) => {
   const { fruits } = data
   res.json(fruits)
 })
@@ -61,9 +91,11 @@ app.delete('/fruits/[id]', (req, res, next) => {
 
 app.post('/fruits', helpers.validate, (req, res, next) => {
   const { fruits } = data
-  const fruits = { id: generateId(), ...req.body }
+  const fruit = { id: generateId(), ... req.body }
 
-  fruits.push(fruits)
+  console.log(fruit)
+
+  fruits.push(fruit)
   res.status(201).json(fruits)
 })
 
